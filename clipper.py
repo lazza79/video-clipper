317f872a96586a671fb0637236964f7809f905b4
Andrea <andrea@rascalpost.com>
2026-04-27 17:07:50 +0100

    Add main window subclass

---
+++ clipper.py
@@ -0,0 +1,22 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class ClipperWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Rascal Clipper")
+        self.resize(1200, 780)
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = ClipperWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
